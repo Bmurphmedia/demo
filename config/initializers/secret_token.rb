19ddec116a635cdfe8b968500e6585408cf24b57
Brendan Murphy <brendan@bmurphmedia.com>
2014-11-01 14:53:19 -0400

    Adding player demo files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '7f824d7d27aed1f8226656d3ffd47d827c8896f501571cb7e54aa2b2250eff05eec46480064f99808b31fb25b3bc83b4a84f8dda564847004c80aef53b205ea9'
